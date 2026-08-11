b852baaf07949b443b07fdb0144dbe929f989ef0
Dinesh Pramanathan <dineshp563@gmail.com>
2026-08-11 11:30:14 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mp = 0;
+        if (prices.length < 2) {
+            return mp;
+        }
+        int sell = 1, buy = 0;
+        while (buy < sell) {
+            int cp = prices[sell] - prices[buy];
+            if (cp > mp) {
+                mp = cp;
+            }
+            if (prices[sell] < prices[buy]) {
+                buy++;
+                if (buy == sell && sell < prices.length - 1) {
+                    sell++;
+                }
+            } else if (sell < prices.length - 1) {
+                sell++;
+            } else {
+                buy++;
+            }
+
+        }
+        return mp;
+    }
+}
